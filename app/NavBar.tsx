7dffccde1a06cc95d703df6ca434889a2f802ea4
SHAFIN SHAIKH <shafinshaikh25@gmail.com>
2024-01-21 04:09:20 +0530

    Built NavBar and React Icons dependency

---
+++ app/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import { FiCodesandbox } from "react-icons/fi";
+
+const NavBar = () => {
+  const links = [
+    {label: 'Dashboard', href: '/'},
+    {label: 'Issues', href: '/issues'},
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FiCodesandbox />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map(link => 
+            <Link 
+            key={link.href} 
+            href={link.href} 
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"> 
+            {link.label}
+            </Link>)}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
