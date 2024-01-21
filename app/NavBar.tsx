fa5b33eea39f33b20f603c232cae51e284422642
SHAFIN SHAIKH <shafinshaikh25@gmail.com>
2024-01-21 21:20:30 +0530

    Update dependencies and add classnames package. added dark color on selection with path

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,12 @@
+'use client';
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FiCodesandbox } from "react-icons/fi";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentpath = usePathname();
   const links = [
     {label: 'Dashboard', href: '/'},
     {label: 'Issues', href: '/issues'},
@@ -17,7 +21,11 @@ const NavBar = () => {
             <Link 
             key={link.href} 
             href={link.href} 
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"> 
+            className={classnames({
+              'text-zinc-900': currentpath === link.href,
+              'text-zinc-500': currentpath !== link.href,
+              'hover:text-zinc-800 transition-colors': true,
+            })}> 
             {link.label}
             </Link>)}
       </ul>
